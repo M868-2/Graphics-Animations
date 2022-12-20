29000bf4a1df14423b46fece5245b229a10ed992
M868-2 <113375937+M868-2@users.noreply.github.com>
2022-12-20 11:21:28 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// JavaScript Code
+// DRAWING BASICS
+
+// SET UP THE CANVAS ADN GRAPHICS CONTEXT
+let cnv = document.getElementById("canv");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// LET'S DRAW SOMETHING USING THE GRAPHICS COMNTEX (CTX)
+// DRAW RECTANGLES
+ctx.strokeRect(50, 20, 150, 50);
