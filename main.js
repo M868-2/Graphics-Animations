c71c16ef15d3f1a2c5c99bbeda892be217cfeb30
M868-2 <113375937+M868-2@users.noreply.github.com>
2022-12-23 10:29:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,4 +8,19 @@ cnv.height = 600;
 
 // LET'S DRAW SOMETHING USING THE GRAPHICS COMNTEX (CTX)
 // DRAW RECTANGLES
+// SET THE OUTLINE AND FILL COLOURS
+// USE ANY VALID CSS COLOURS: NAME, RGB (), RGBA (), HEX #FF0033
+ctx.strokeStyle = "purple";
 ctx.strokeRect(50, 20, 150, 50);
+
+ctx.fillStyle = "pink";
+ctx.fillRect(225, 150, 200, 200);
+
+// DRAW TEXT
+ctx.font = "40px Times New Roman";
+ctx.fillStyle = "black";
+ctx.fillText("I Found You :)", 300, 50);
+
+ctx.font = "30px Arial";
+ctx.strokeStyle = "rgb(0, 20, 40)";
+ctx.strokeText("Save me", 300, 100);
